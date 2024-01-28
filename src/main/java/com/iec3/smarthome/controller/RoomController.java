46a6d2f15159b57a7307a698f27f5c00ad417269
Feri Carnu <fericarnu@gmail.com>
2024-01-28 17:59:27 +0200

    preliminary html for rooms page

--- src/main/java/com/iec3/smarthome/controller/RoomController.java
+++ src/main/java/com/iec3/smarthome/controller/RoomController.java
@@ -4,12 +4,13 @@ import com.iec3.smarthome.entity.Room;
 import com.iec3.smarthome.entity.RoomDeviceList;
 import com.iec3.smarthome.service.DeviceService;
 import com.iec3.smarthome.service.RoomService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@RestController
+@Controller
 @RequestMapping(path = "rooms")
 public class RoomController {
 
@@ -22,12 +23,12 @@ public class RoomController {
     }
 
     @GetMapping
-    public ArrayList<RoomDeviceList> listRooms(Model model) {
+    public String listRooms(Model model) {
         var DeviceL=new ArrayList<RoomDeviceList>();
         var allRooms = roomService.getRoom();
         allRooms.forEach(room -> DeviceL.add(roomService.getDevices(room.getId())));
-        model.addAttribute("rooms", allRooms);
-        return DeviceL;
+        model.addAttribute("list", DeviceL);
+        return "rooms";
     }
 
     @GetMapping("{id}")
